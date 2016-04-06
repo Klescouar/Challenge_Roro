ca0227576a9234f5f31ab511c3b619af11f622d2
Poncho <lescouarneckevin@yahoo.fr>
2016-04-06 18:47:31 +0200

    Non vraiment je comprends pas mais ça marche

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+  
+function modifValues(){
+
+    var val = $('#progress progress').attr('value');
+
+    if(val>=100){val=1;}
+
+    var newVal = val*1+0.25;
+
+    var txt = Math.floor(newVal)+'min';
+
+
+
+    $('#progress progress').attr('value',newVal).text(txt);
+
+    $('#progress strong').html(txt);
+
+}
+
+setInterval(function(){ modifValues(); },40);
